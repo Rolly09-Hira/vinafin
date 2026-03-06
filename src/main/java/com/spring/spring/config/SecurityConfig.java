091693827398dd3d6ff6add39b9ee52240e7ec5c
Rolly09-Hira <rollyandriamahery911@gmail.com>
2026-03-06 21:48:31 +0300

    Update SecurityConfig.java

--- src/main/java/com/spring/spring/config/SecurityConfig.java
+++ src/main/java/com/spring/spring/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://**.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://**.vercel.app", "https://*.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
