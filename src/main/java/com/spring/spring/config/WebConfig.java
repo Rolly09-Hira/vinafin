50cbd02f58ca0fbceca6e762d16896dc75240202
Rolly09-Hira <rollyandriamahery911@gmail.com>
2026-03-12 07:30:23 +0300

    Update WebConfig.java

--- src/main/java/com/spring/spring/config/WebConfig.java
+++ src/main/java/com/spring/spring/config/WebConfig.java
@@ -9,14 +9,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Servir depuis le dossier uploads à la racine (si présent)
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/")
-                .setCachePeriod(0);
+        // // Servir depuis le dossier uploads à la racine (si présent)
+        // registry.addResourceHandler("/uploads/**")
+        //         .addResourceLocations("file:uploads/")
+        //         .setCachePeriod(0);
         
-        // Servir depuis le dossier static/uploads dans le JAR (garanti)
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("classpath:/static/uploads/")
-                .setCachePeriod(0);
+        // // Servir depuis le dossier static/uploads dans le JAR (garanti)
+        // registry.addResourceHandler("/uploads/**")
+        //         .addResourceLocations("classpath:/static/uploads/")
+        //         .setCachePeriod(0);
     }
 }
